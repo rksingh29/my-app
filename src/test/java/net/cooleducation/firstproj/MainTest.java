75a78de6568d9622a23a69fae26e3084dd86b099
Rajesh Singh <rsingh@carbonite.com>
2020-05-02 00:34:25 +0530

    2 commit

---
+++ src/test/java/net/cooleducation/firstproj/MainTest.java
@@ -0,0 +1,15 @@
+package net.cooleducation.firstproj;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+class MainTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
